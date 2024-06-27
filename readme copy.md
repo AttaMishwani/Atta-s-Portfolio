efba06a2d2f58bba66f22c38288ac9e4df3293e0
Atta Ur Rehman <attamishwani2006@gmail.com>
2024-06-28 00:14:42 +0500

    first commit

---
+++ readme copy.md
@@ -0,0 +1,7 @@
+body{
+    background-color: #010B13;
+    width: 100%;
+    height: 100%;
+}
+
+create a picture of a web developer posing on his right shoulder for picture his hands are in lock the image width and height should be 500px and image quality should be the best  the image corner should not be round
---
+++ script.js
@@ -0,0 +1,26 @@
+let toggleBtn = document.getElementById("toggle-btn");
+let toggleclosebtn = document.getElementById("xmark");
+let menu = document.querySelector(".menu");
+const menuLinks = document.querySelectorAll(".menu-link");
+
+
+toggleBtn.addEventListener("click", () => {
+    console.log("working")
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+    } else {
+        menu.classList.add("active");
+    }
+});
+
+toggleclosebtn.addEventListener("click",()=>{
+    menu.classList.remove("active");
+});
+
+
+
+menuLinks.forEach(link =>{
+    link.addEventListener("click",()=>{
+        menu.classList.remove("active")
+    })
+})
