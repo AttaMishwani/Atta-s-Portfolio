21b219722318caf3a4cde2055dbb9c0a2066adf9
Atta Ur Rehman <145256710+Atta2006@users.noreply.github.com>
2024-07-11 12:58:34 +0500

    added some animations

--- public/gsap.js
+++ public/gsap.js
@@ -1,28 +1,47 @@
 
-gsap.from(".navbar",{
-   delay:0.4,
-   opacity:0,
-   duration:0.5,
-   y:-60
+
+// Navbar animation
+gsap.from(".navbar", {
+  delay: 0.4,
+  opacity: 0,
+  duration: 0.5,
+  y: -60
 });
 
-gsap.from(".home-txt",{
-  delay:0.4,
-  opacity:0,
-  duration:0.5,
-  x:-100
+// Home text animation
+gsap.from(".home-txt", {
+  delay: 0.4,
+  opacity: 0,
+  duration: 0.5,
+  x: -100
 });
 
-gsap.from(".about-left",{
-  delay:0.4,
-  opacity:0,
-  duration:0.5,
-  x:-100,
+// About section animations
+gsap.from(".about-right-txt", {
+  delay: 2,
+  opacity: 0,
+  duration: 0.5,
+  x: 100,
   scrollTrigger: {
-    trigger: ".about-left",
+    trigger: ".about-right-txt",
     scroller: "body",
-    start: "top 60%",    
-    end: "top 55%",
-    scrub: 2
+    start: "top 50%"
   }
-})
+});
+
+gsap.from(".about-left-img", {
+  delay: 2,
+  opacity: 0,
+  duration: 0.5,
+  x: -100,
+  scrollTrigger: {
+    trigger: ".about-left-img",
+    scroller: "body",
+    start: "top 50%"
+  }
+});
+
+
+
+// Register ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
